66d5e66084ad73070b31f2d70d55145dbc149de4
lincw <56734434+lincww@users.noreply.github.com>
2023-02-02 00:56:02 +0800

    update

--- serverless/src/trigger/index.ts
+++ serverless/src/trigger/index.ts
@@ -1,27 +1,39 @@
 import {Env} from ".."
-import {User} from "../types";
+import {User, Star, rawJsonDataFromGithub} from "../types";
 
 const trigger = async (env: Env): Promise<void> => {
   const KV = env.starsTrackerKV
   const userInfos = await KV.get<Array<User>>("userInfo", "json")
   if (!userInfos) return
   for (const user of userInfos) {
-    const lastStars = await KV.get<Array>(`lastestStars:${user.name}`,"json")
-    if 
+    const latestStars = await KV.get<Array<Star>>(`latestStars:${user.name}`, "json")
     const headers = {
       Accept: "application/vnd.github+json",
-      Authorization: `Bearer ${user.token}`,
+      // Authorization: `Bearer ${user.token}`,
+      Authorization: `Bearer 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`,
       "X-GitHub-Api-Version": "2022-11-28",
     }
     let page = 1
     let is_end = false
-    let stars = []
+    let stars:rawJsonDataFromGithub[] = []
     while (!is_end) {
       const resp = await fetch(`https://api.github.com/user/starred?per_page=100&page=${page}`, {headers})
-      const data = resp.json
-      stars.push(data)
+      const data: [] = await resp.json()
+      stars.push(...data)
       if (data.length < 100) is_end = true
     }
+    let newStars:Star[] = stars.map((data):Star=>{return {
+      starredAt: data.starred_at,
+      name: data.repo.name,
+      fullName: data.repo.full_name,
+      description: data.repo.description,
+      counts: {
+        openIssues: data.repo.open_issues_count
+      }
+
+    }})
+    // Diff
+
   }
 }
 
